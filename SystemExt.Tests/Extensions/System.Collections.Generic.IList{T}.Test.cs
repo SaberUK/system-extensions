f94ba4e2f4c5ae512c3efad77b8b1ad4b3829416
Peter Powell <petpow@saberuk.com>
2014-11-27 21:58:53 +0000

    Implement some useful IList{T} extensions.

---
+++ SystemExt.Tests/Extensions/System.Collections.Generic.IList{T}.Test.cs
@@ -0,0 +1,76 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System.Collections.Generic;
+using SystemExt.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SystemExt.Tests.Extensions
+{
+
+    /// <summary>
+    /// Implements tests for the <see cref="System_Collections_Generic_IList_T"/> class.
+    /// </summary>
+    [TestClass]
+    public sealed class System_Collections_Generic_IList_T_Test
+    {
+
+        /// <summary>
+        /// Test the <see cref="System_Collections_Generic_IList_T.AddRange{T}"/> extension method.
+        /// </summary>
+        [TestMethod]
+        public void AddRange()
+        {
+            var testDataParent = new List<int>
+            {
+                    1, 2, 3, 4, 5
+            };
+            var testDataChild = new List<int>
+            {
+                    6, 7, 8, 9, 10
+            };
+
+            testDataParent.AddRange(testDataChild);
+
+            // Ensure the parent list has the right amount of elements.
+            Assert.AreEqual(testDataParent.Count, 10);
+
+            // Ensure all of the child elements are in the parent.
+            foreach (var element in testDataChild)
+                Assert.IsTrue(testDataParent.Contains(element));
+        }
+
+        /// <summary>
+        /// Test the <see cref="System_Collections_Generic_IList_T.ElementAtOrValue{T}"/> extension method.
+        /// </summary>
+        [TestMethod]
+        public void ElementAtOrValue()
+        {
+            var testData = new List<int>
+            {
+                    1, 2, 3, 4, 6
+            };
+
+            // Ensure that indexes in range return the correct values
+            for (var i = 0; i < testData.Count; i++)
+                Assert.AreEqual(testData[i], testData.ElementAtOrValue(i, -1));
+
+            // Ensure that indexes which are out of range return the default value.
+            Assert.AreEqual(testData.ElementAtOrValue(int.MinValue, -1), -1);
+            Assert.AreEqual(testData.ElementAtOrValue(int.MaxValue, -1), -1);
+        }
+    }
+}
---
+++ SystemExt/Extensions/System.Collections.Generic.IList{T}.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System.Collections.Generic;
+
+namespace SystemExt.Extensions
+{
+
+    /// <summary>
+    /// Extension methods for the <see cref="IList{T}"/> struct.
+    /// </summary>
+    public static class System_Collections_Generic_IList_T
+    {
+
+        /// <summary>
+        /// Adds the elements of the specified enumerable to the end of the <see cref="IList{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the element in the collection.
+        /// </typeparam>
+        /// <param name="self">
+        /// The <see cref="IList{T}"/> to add the elements in <paramref name="enumerable"/> tp
+        /// </param>
+        /// <param name="enumerable">
+        /// An enumerable list of <typeparamref name="T"/> to add to <paramref name="self"/>.
+        /// </param>
+        public static void AddRange<T>(this IList<T> self, IEnumerable<T> enumerable)
+        {
+            foreach (var item in enumerable)
+                self.Add(item);
+        }
+
+        /// <summary>
+        /// Returns the element at a specified index in a sequence or the given value if the
+        /// index is out of range.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the elements in <paramref name="self"/>.
+        /// </typeparam>
+        /// <param name="self">
+        /// An <see cref="IList{T}"/> to return an element from.
+        /// </param>
+        /// <param name="index">
+        /// The zero-based index of the element to retrieve.
+        /// </param>
+        /// <param name="defaultValue">
+        /// The value to return if the index is out of range.
+        /// </param>
+        /// <returns>
+        /// The element requested from <paramref name="self"/> or <paramref name="defaultValue"/>
+        /// if it does not exist.
+        /// </returns>
+        public static T ElementAtOrValue<T>(this IList<T> self, int index, T defaultValue)
+        {
+            return index >= 0 && index < self.Count ? self[index] : defaultValue;
+        }
+    }
+}
