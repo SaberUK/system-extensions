3a0afe190e2e15dcb81ebfe38b0b6fee54020b1a
Peter Powell <petpow@saberuk.com>
2015-02-11 23:42:03 +0000

    Add an extension method which repeats a string n times.

---
+++ SystemExt.Tests/Extensions/System.String.Test.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2015 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using SystemExt.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SystemExt.Tests.Extensions
+{
+
+    /// <summary>
+    /// Implements tests for the <see cref="System_String"/> class.
+    /// </summary>
+    [TestClass]
+    public sealed class System_String_Test
+    {
+
+        /// <summary>
+        /// Test the <see cref="System_String.Repeat"/> extension method.
+        /// </summary>
+        [TestMethod]
+        public void Repeat()
+        {
+            // Ensure we can repeat a string correctly.
+            Assert.AreEqual("ABABABABAB", "AB".Repeat(5));
+        }
+    }
+}
---
+++ SystemExt/Extensions/System.String.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2015 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System;
+using System.Text;
+
+namespace SystemExt.Extensions
+{
+
+    /// <summary>
+    /// Extension methods for the <see cref="String"/> class.
+    /// </summary>
+    public static class System_String
+    {
+
+        /// <summary>
+        /// Create a new <see cref="String"/> which is <paramref name="self"/> repeated
+        /// <paramref name="times"/> times.
+        /// </summary>
+        /// <param name="self">
+        /// An instance of the <see cref="String"/> class.
+        /// </param>
+        /// <param name="times">
+        /// The number of times to repeat <see cref="times"/>.
+        /// </param>
+        /// <returns>
+        /// <paramref name="self"/> repeated <paramref name="times"/> times.
+        /// </returns>
+        public static string Repeat(this string self, uint times)
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < times; i++)
+                builder.Append(self);
+
+            return builder.ToString();
+        }
+    }
+}
