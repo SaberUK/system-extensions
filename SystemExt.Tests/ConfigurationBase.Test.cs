93fbb936032ad9ee543d1e8fa335b265419da0d0
Peter Powell <petpow@saberuk.com>
2016-01-04 06:02:26 +0000

    Add a base class for serializable configuration data.

---
+++ SystemExt.Tests/ConfigurationBase.Test.cs
@@ -0,0 +1,95 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014-2016 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SystemExt.Tests
+{
+
+    /// <summary>
+    /// Implements tests for the <see cref="ConfigurationBase{T}"/> class.
+    /// </summary>
+    [TestClass]
+    public sealed class ConfigurationBase_Test
+    {
+
+        /// <summary>
+        /// A configuration class that we use for testing.
+        /// </summary>
+        public sealed class TestConfiguration : ConfigurationBase<TestConfiguration>
+        {
+
+            /// <summary>
+            /// Whether we should foo.
+            /// </summary>
+            public bool Foo { get; set; }
+
+            /// <summary>
+            /// How much we should bar.
+            /// </summary>
+            public int Bar { get; set; }
+
+            /// <summary>
+            /// The name of what we should baz.
+            /// </summary>
+            public string Baz { get; set; }
+
+        }
+
+        /// <summary>
+        /// The file which contains test data.
+        /// </summary>
+        private const string TestFile = "TestData/ConfigurationBase.xml";
+
+        /// <summary>
+        /// Test the <see cref="ConfigurationBase{T}.Load"/> method.
+        /// </summary>
+        [DeploymentItem(ConfigurationBase_Test.TestFile)]
+        [TestMethod]
+        public void Load()
+        {
+            // Ensure we can read data correctly from a file.
+            var configuration = TestConfiguration.Load(ConfigurationBase_Test.TestFile);
+            Assert.IsFalse(configuration.Foo);
+            Assert.AreEqual(-1, configuration.Bar);
+            Assert.AreEqual("Nothing", configuration.Baz);
+        }
+
+        /// <summary>
+        /// Test the <see cref="ConfigurationBase{T}.Save"/> method.
+        /// </summary>
+        [DeploymentItem(ConfigurationBase_Test.TestFile)]
+        [TestMethod]
+        public void Save()
+        {
+            // Create a dummy configuration and write it to a file.
+            var configuration = new TestConfiguration
+            {
+                Foo = false,
+                Bar = -1,
+                Baz = "Nothing"
+            };
+            var location = Path.GetTempFileName();
+            configuration.Save(location);
+
+            var expected = File.ReadAllText(ConfigurationBase_Test.TestFile);
+            var actual = File.ReadAllText(location);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ SystemExt/ConfigurationBase.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014-2016 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SystemExt
+{
+
+    /// <summary>
+    /// Base class for a class containing configuration properties.
+    /// </summary>
+    public abstract class ConfigurationBase<T>
+        where T : ConfigurationBase<T>, new()
+    {
+
+        /// <summary>
+        /// Load the configuration from disk.
+        /// </summary>
+        /// <param name="path">
+        /// The location to load from.
+        /// </param>
+        /// <param name="missingOkay">
+        /// Whether a new configuration should be created if the file doesn't exist.
+        /// </param>
+        /// <returns>
+        /// A new instance of the <see cref="T"/> class.
+        /// </returns>
+        public static T Load(string path, bool missingOkay = true)
+        {
+            // If the file doesn't exist then we don't need to load it.
+            if (!File.Exists(path) && missingOkay)
+                return new T();
+
+            // Create a serializer instance and load the file.
+            var serializer = new XmlSerializer(typeof(T));
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                return (T)serializer.Deserialize(stream);
+            }
+        }
+
+        /// <summary>
+        /// Write the configuration to disk.
+        /// </summary>
+        /// <param name="path">
+        /// The location to save to.
+        /// </param>
+        public void Save(string path)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                serializer.Serialize(stream, this);
+            }
+        }
+    }
+}
