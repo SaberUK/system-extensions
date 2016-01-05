a1eb2a489ed6a2e4853c26581cc663e4bf602055
Peter Powell <petpow@saberuk.com>
2016-01-05 14:40:42 +0000

    Add a type for representing optional values.
    This is similar to Nullable<T> but for all types and with more functionality.
    
    This subsumes and replaces the TryCatch extension method.

---
+++ SystemExt.Tests/Option{T}.Test.cs
@@ -0,0 +1,112 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014-2016 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SystemExt.Tests
+{
+
+    /// <summary>
+    /// Implements tests for the <see cref="Option{T}"/> class.
+    /// </summary>
+    [TestClass]
+    public sealed class SystemExt_Option_T_Test
+    {
+
+        /// <summary>
+        /// Test the <see cref="Option{T}.Create"/> method.
+        /// </summary>
+        [TestMethod]
+        public void Create()
+        {
+            const int value = 42;
+            var result = Option<int>.Create(value);
+
+            // Ensure that the option has a value.
+            Assert.IsTrue(result.HasValue);
+
+            // Ensure that we can retrieve the wrapped value.
+            Assert.AreEqual(value, result.Get());
+            Assert.AreEqual(value, result.GetOr(58));
+            Assert.AreEqual(value, result.GetOr(() => 58));
+
+            // Ensure that the and operation is called when we have a value.
+            Assert.AreEqual(84, result.AndThen(number => number * 2).Get());
+        }
+
+        /// <summary>
+        /// Test the <see cref="Option{T}.FromNullable{T2}"/> method.
+        /// </summary>
+        [TestMethod]
+        public void FromNullable()
+        {
+            const int value = 42;
+
+            // Ensure that a Nullable<T> with a value creates a valid Option<T>.
+            int? hasValue = value;
+            Assert.AreEqual(Option<int>.Create(value).Get(), Option<int>.FromNullable(hasValue).Get());
+
+            // Ensure that a Nullable<T> with no value returns None.
+            int? noValue = null;
+            Assert.AreEqual(Option<int>.None, Option<int>.FromNullable(noValue));
+        }
+
+        /// <summary>
+        /// Test the <see cref="Option{T}.FromThrowingFunc{T2}"/> method.
+        /// </summary>
+        [TestMethod]
+        public void FromThrowingFunc()
+        {
+            const int value = 42;
+
+            // Ensure that a normal return value creates a valid Option<T>.
+            Assert.AreEqual(value, Option<int>.FromThrowingFunc(() => value).Get());
+
+            // Ensure that an exception being thrown returns None.
+            Assert.AreEqual(Option<int>.None, Option<int>.FromThrowingFunc<int>(() => { throw new Exception(); }));
+        }
+
+        /// <summary>
+        /// Test the <see cref="Option{T}.None"/> value.
+        /// </summary>
+        [TestMethod]
+        public void None()
+        {
+            var result = Option<int>.None;
+
+            // Ensure that the option has no value.
+            Assert.IsFalse(result.HasValue);
+
+            // Ensure that an exception is thrown when trying to retrieve the value.
+            try
+            {
+                result.Get();
+                Assert.Fail("Calling Get() on Option{T}.None did not throw an InvalidOperationException!");
+            }
+            catch (InvalidOperationException) { }
+
+            // Ensure that we get the default values.
+            const int value = 42;
+            Assert.AreEqual(value, result.GetOr(value));
+            Assert.AreEqual(value, result.GetOr(() => value));
+
+            // Ensure that the and operation is not called when we have no value.
+            Assert.AreEqual(Option<int>.None, result.AndThen(number => number * 2));
+        }
+    }
+}
---
+++ SystemExt/Option{T}.cs
@@ -0,0 +1,195 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014-2016 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System;
+
+namespace SystemExt
+{
+
+    /// <summary>
+    /// Represents the result of a function which may not exist.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type which this <see cref="Option{T}"/> wraps.
+    /// </typeparam>
+    public sealed class Option<T>
+    {
+
+        /// <summary>
+        /// Whether this <see cref="Option{T}"/> has a value.
+        /// </summary>
+        public readonly bool HasValue;
+
+        /// <summary>
+        /// The instance returned when no value is available.
+        /// </summary>
+        public static readonly Option<T> None = new Option<T>(); 
+
+        /// <summary>
+        /// The value this <see cref="Option{T}"/> is wrapping.
+        /// </summary>
+        private readonly T Value;
+
+        /// <summary>
+        /// Constructor used by the <see cref="Option{T}.None"/> instance.
+        /// </summary>
+        private Option()
+        {
+            this.HasValue = false;
+            this.Value = default(T);
+        }
+
+        /// <summary>
+        /// Constructor used when there is a value.
+        /// </summary>
+        /// <param name="value">
+        /// The value this <see cref="Option{T}"/> is wrapping.
+        /// </param>
+        private Option(T value)
+        {
+            this.HasValue = true;
+            this.Value = value;
+        }
+        
+        /// <summary>
+        /// Implicitly conversion from <typeparamref name="T"/> to <see cref="Option{T}"/>.
+        /// </summary>
+        /// <param name="value">
+        /// The value the <see cref="Option{T}"/> should wrap.
+        /// </param>
+        /// <returns>
+        /// A new instance of the <see cref="Option{T}"/> class.
+        /// </returns>
+        public static implicit operator Option<T>(T value)
+        {
+            return new Option<T>(value);
+        }
+
+        /// <summary>
+        /// If this <see cref="Option{T}"/> is not <see cref="None"/> then call the function and return the result.
+        /// Otherwise, return <see cref="Option{T2}.None"/>.
+        /// </summary>
+        /// <typeparam name="T2">
+        /// The return type of <paramref name="func"/>.
+        /// </typeparam>
+        /// <param name="func">
+        /// The function to call if this <see cref="Option{T}"/> is not null.
+        /// </param>
+        /// <returns>
+        /// Either an <see cref="Option{T2}"/> which either wraps the result of the function call, or <see cref="Option{T2}.None"/>.
+        /// </returns>
+        public Option<T2> AndThen<T2>(Func<T, T2> func)
+        {
+            return this.HasValue ? Option<T2>.FromThrowingFunc(() => func(this.Value)) : Option<T2>.None;
+        }
+
+        /// <summary>
+        /// Wraps a value in an <see cref="Option{T}"/>.
+        /// </summary>
+        /// <param name="value">
+        /// The value the <see cref="Option{T}"/> should wrap.
+        /// </param>
+        /// <returns>
+        /// A new instance of the <see cref="Option{T}"/> class.
+        /// </returns>
+        public static Option<T> Create(T value)
+        {
+            return new Option<T>(value);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="Nullable{T2}"/> value to an <see cref="Option{T2}"/>.
+        /// </summary>
+        /// <typeparam name="T2">
+        /// The type which is being wrapped by the <see cref="Nullable{T2}"/>.
+        /// </typeparam>
+        /// <param name="value">
+        /// A nullable value to convert to an <see cref="Option{T2}"/>.
+        /// </param>
+        /// <returns>
+        /// A new option which wraps the value in <paramref name="value"/>.
+        /// </returns>
+        public static Option<T2> FromNullable<T2>(T2? value)
+            where T2 : struct
+        {
+            return value.HasValue ? new Option<T2>(value.Value) : Option<T2>.None;
+        }
+
+        /// <summary>
+        /// Execute a function which might throw and wraps the result in an Option{T2}.
+        /// </summary>
+        /// <param name="func">
+        /// The function to wrap the result of.
+        /// </param>
+        /// <returns>
+        /// The result of executing <paramref name="func"/> or <see cref="None"/> if it throws an exception.
+        /// </returns>
+        public static Option<T2> FromThrowingFunc<T2>(Func<T2> func)
+        {
+            try
+            {
+                var result = func();
+                return new Option<T2>(result);
+            }
+            catch (Exception)
+            {
+                return Option<T2>.None;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the wrapped value if it exists and throws an <see cref="InvalidOperationException"/> if it does not.
+        /// </summary>
+        /// <returns>
+        /// The value which is wrapped by this <see cref="Option{T}"/>.
+        /// </returns>
+        public T Get()
+        {
+            if (!this.HasValue)
+                throw new InvalidOperationException(string.Format("Get was called on an empty SystemEx.Option<{0}>!", typeof(T)));
+
+            return this.Value;
+        }
+
+        /// <summary>
+        /// Retrieves the wrapped value if it exists or the specified value if it does not.
+        /// </summary>
+        /// <param name="value">
+        /// The value to return if there is no wrapped value.
+        /// </param>
+        /// <returns>
+        /// The value which is wrapped by this <see cref="Option{T}"/> or <paramref name="value"/> if it does not exist.
+        /// </returns>
+        public T GetOr(T value)
+        {
+            return this.HasValue ? this.Value : value;
+        }
+
+        /// <summary>
+        /// Retrieves the wrapped value if it exists or the return value of the specified function if it does not.
+        /// </summary>
+        /// <param name="func">
+        /// The function to return the result of if there is no wrapped value.
+        /// </param>
+        /// <returns>
+        /// The value which is wrapped by this <see cref="Option{T}"/> or the result of <paramref name="func"/> if it does not exist.
+        /// </returns>
+        public T GetOr(Func<T> func)
+        {
+            return this.HasValue ? this.Value : func();
+        }
+    }
+}
