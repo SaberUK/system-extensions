cb76501e25bb73b27042a59fb4a0382dfc312525
Peter Powell <petpow@saberuk.com>
2016-09-07 04:21:18 +0100

    Add helper methods for determining whether an IP address is valid.

---
+++ SystemExt.Tests/Network/NetworkHelper.Test.cs
@@ -0,0 +1,85 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014-2016 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using SystemExt.Network;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SystemExt.Tests.Network
+{
+
+    /// <summary>
+    /// Implements tests for the <see cref="NetworkHelper"/> class.
+    /// </summary>
+    [TestClass]
+    public sealed class NetworkHelper_Test
+    {
+
+        /// <summary>
+        /// Test the <see cref="NetworkHelper.IsValidAddress"/> method.
+        /// </summary>
+        [TestMethod]
+        public void IsValidAddress()
+        {
+            // Ensure that a valid IPv4 results in true being returned.
+            Assert.IsTrue(NetworkHelper.IsValidAddress("127.0.0.1"));
+
+            // Ensure that a valid IPv6 results in true being returned.
+            Assert.IsTrue(NetworkHelper.IsValidAddress("::1"));
+
+            // Ensure that invalid data results in false being returned.
+            Assert.IsFalse(NetworkHelper.IsValidAddress("192.168.1."));
+            Assert.IsFalse(NetworkHelper.IsValidAddress("example.com"));
+            Assert.IsFalse(NetworkHelper.IsValidAddress("invalid address"));
+        }
+
+        /// <summary>
+        /// Test the <see cref="NetworkHelper.IsValidAddressV4"/> method.
+        /// </summary>
+        [TestMethod]
+        public void IsValidAddressV4()
+        {
+            // Ensure that a valid IPv4 results in true being returned.
+            Assert.IsTrue(NetworkHelper.IsValidAddressV4("127.0.0.1"));
+
+            // Ensure that a valid IPv6 results in false being returned.
+            Assert.IsFalse(NetworkHelper.IsValidAddressV4("::1"));
+
+            // Ensure that invalid data results in false being returned.
+            Assert.IsFalse(NetworkHelper.IsValidAddressV4("192.168.1."));
+            Assert.IsFalse(NetworkHelper.IsValidAddressV4("example.com"));
+            Assert.IsFalse(NetworkHelper.IsValidAddressV4("invalid address"));
+        }
+
+        /// <summary>
+        /// Test the <see cref="NetworkHelper.IsValidAddressV6"/> method.
+        /// </summary>
+        [TestMethod]
+        public void IsValidAddressV6()
+        {
+            // Ensure that a valid IPv6 results in true being returned.
+            Assert.IsTrue(NetworkHelper.IsValidAddressV6("::1"));
+
+            // Ensure that a valid IPv4 results in false being returned.
+            Assert.IsFalse(NetworkHelper.IsValidAddressV6("127.0.0.1"));
+
+            // Ensure that invalid data results in false being returned.
+            Assert.IsFalse(NetworkHelper.IsValidAddressV6("192.168.1."));
+            Assert.IsFalse(NetworkHelper.IsValidAddressV6("example.com"));
+            Assert.IsFalse(NetworkHelper.IsValidAddressV6("invalid address"));
+        }
+    }
+}
---
+++ SystemExt/Network/NetworkHelper.cs
@@ -0,0 +1,80 @@
+﻿/**
+ * System Extensions
+ *
+ *   Copyright (C) 2014-2016 Peter "SaberUK" Powell <petpow@saberuk.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace SystemExt.Network
+{
+
+    /// <summary>
+    /// Implements helper methods for network use.
+    /// </summary>
+    public static class NetworkHelper
+    {
+
+        /// <summary>
+        /// Determines whether <see cref="text"/> contains a valid IP address.
+        /// </summary>
+        /// <param name="text">
+        /// The value to validate as an IPv4 address.
+        /// </param>
+        /// <returns>
+        /// True if <paramref name="text"/> is a valid IP address. Otherwise, false.
+        /// </returns>
+        public static bool IsValidAddress(string text)
+        {
+            IPAddress address;
+            return IPAddress.TryParse(text, out address);
+        }
+
+        /// <summary>
+        /// Determines whether <paramref name="text"/> contains a valid IPv4 address.
+        /// </summary>
+        /// <param name="text">
+        /// The value to validate as an IPv4 address.
+        /// </param>
+        /// <returns>
+        /// True if <paramref name="text"/> is a valid IPv4 address. Otherwise, false.
+        /// </returns>
+        public static bool IsValidAddressV4(string text)
+        {
+            IPAddress address;
+            if (!IPAddress.TryParse(text, out address))
+                return false;
+
+            return address.AddressFamily == AddressFamily.InterNetwork;
+        }
+
+        /// <summary>
+        /// Determines whether <paramref name="text"/> contains a valid IPv6 address.
+        /// </summary>
+        /// <param name="text">
+        /// The value to validate as an IPv4 address.
+        /// </param>
+        /// <returns>
+        /// 
+        /// </returns>
+        public static bool IsValidAddressV6(string text)
+        {
+            IPAddress address;
+            if (!IPAddress.TryParse(text, out address))
+                return false;
+
+            return address.AddressFamily == AddressFamily.InterNetworkV6;
+        }
+    }
+}
